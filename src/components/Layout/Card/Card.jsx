94b60850ddaa35beb0ad2cd75b40e023fcb896cd
andres <andres2345v@gmail.com>
2025-03-17 07:48:52 -0500

    Eleven commit, Uptate for movile

--- src/components/Layout/Card/Card.jsx
+++ src/components/Layout/Card/Card.jsx
@@ -1,17 +1,17 @@
 import '../Card/card.css'
-export const Card = ({}) =>{
+export const Card = ({imge = '../src/assets/images/icon/python.png', text ='Python', porcent='100%',  estilos='progress-bar-p' }) =>{
     return (
 
         <div className="card">
         <div className="card-image">
-          <img src="../src/assets/images/icon/python.png" alt="python"/>
+          <img src={imge} alt={text}/>
         </div>
         <div className='card-content'>
-        <h3 className="card-title">Python</h3>
+        <h3 className="card-title">{text}</h3>
         <div className='progress-container'>
           <div class="progress">
-            <div class="progress-bar">
-                <span class="progress-bar-text">60%</span>
+            <div class={estilos}>
+                <span class="progress-bar-text">{porcent}</span>
             </div>
           </div>
         </div>
--- src/components/Pages/Profile/Profile.jsx
+++ src/components/Pages/Profile/Profile.jsx
@@ -3,10 +3,20 @@ import { Card } from '../../Layout/Card/Card'
 
 export const Profile = () => {
   return (
-    <>
-      <h2>I'm Profile</h2>
-
-      <Card />
-    </>
+    <div className='Profile'>
+      <h2>Profile</h2>
+      <div className='cards'>
+        <Card />
+        <Card imge='src/assets/images/icon/css.png' text='CSS' porcent='55%' estilos='progress-bar-c'/>
+        <Card imge='src/assets/images/icon/figma.png' text='Figma' porcent='50%' estilos='progress-bar-f'/>
+        <Card imge='src/assets/images/icon/html.png' text='HTML' porcent='80%' estilos='progress-bar-h'/>
+        <Card imge='src/assets/images/icon/java.png' text='JAVA' porcent='96%' estilos='progress-bar-j'/>
+        <Card imge='src/assets/images/icon/JavaScript.png' text='JavaScript' porcent='90%' estilos='progress-bar-s'/>
+        <Card imge='src/assets/images/icon/mongp.png' text='Mongo' porcent='30%' estilos='progress-bar-m'/>
+        <Card imge='src/assets/images/icon/node.png' text='Node' porcent='25%' estilos='progress-bar-n'/>
+        <Card imge='src/assets/images/icon/react.png' text='React' porcent='45%' estilos='progress-bar-r'/>
+      </div>
+      
+    </div>
   )
 }
