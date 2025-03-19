8a889de290fa86bc215f2473c6b9425d974fa0c0
andres <andres2345v@gmail.com>
2025-03-19 15:37:50 -0500

    Sixteen commit, first update Experience

--- src/components/Pages/Experience/Experience.jsx
+++ src/components/Pages/Experience/Experience.jsx
@@ -5,8 +5,12 @@ import { Card_container } from '../../Layout/Card-container/Card-container'
 export const Experience = () => {
   return (
     <div className='expi'>
-      <h2>Experienca</h2>
 
+      <Card_container />
+      <Card_container />
+      <Card_container />
+      <Card_container />
+      <Card_container />
       <Card_container />
     </div>
   )
