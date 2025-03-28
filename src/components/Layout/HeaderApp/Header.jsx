1c7ca62de42b0903b2b457a2d81e74a85bdb2702
andres <andres2345v@gmail.com>
2025-03-27 20:54:12 -0500

    ensayo 6

--- src/components/Layout/HeaderApp/Header.jsx
+++ src/components/Layout/HeaderApp/Header.jsx
@@ -1,9 +1,10 @@
 import { NavBar } from "../NavBarApp/NavBar"
+import logoName from "../../../assets/images/logo-name.jpeg"
 
 export const Header = () => {
     return(
         <header className="header">
-            <img className="Logo-name" src="./src/assets/images/logo-name.jpeg" alt="LOGO" />
+            <img className="Logo-name" src={logoName} alt="LOGO" />
             <NavBar />
         </header>
     )
